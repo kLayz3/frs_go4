0fa70fa8578b41d899a855bb87b3838e3d6b05d4
vratislav.chudoba <chudoba@jinr.ru>
2024-02-14 01:14:22 +0100

    Reading of pedestals from root file added.

--- src/foot/TFOOTContainer.h
+++ src/foot/TFOOTContainer.h
@@ -6,35 +6,34 @@
 #include "TObject.h"
 #include <vector>
 
-class TFOOTContainer :public TObject
+class TFOOTContainer : public TObject
 {
 public:
   TFOOTContainer();
   virtual ~TFOOTContainer();
 
-  void ReadCalib(const char * file);
-  void Set(UInt_t * data);
+  void ReadCalib(const char *file);
+  void ReadCalibFromROOTfile(const char *file, Int_t i);
+  void Set(UInt_t *data);
 
   void Calib();
   void EvalMult();
   void FindCluster();
   Double_t Amp[FOOT_CHN];
-  Bool_t bad[FOOT_CHN]; //!
+  Bool_t bad[FOOT_CHN];  //!
   Double_t C0[FOOT_CHN]; //!
-  // Double_t C1[FOOT_CHN]; //! 
+  // Double_t C1[FOOT_CHN]; //!
   Double_t threshold[FOOT_CHN]; //!
-  UInt_t mult; //number of strips with deposit
-  Double_t  Ampnth[FOOT_CHN]; //[mult]
-  Int_t  strip[FOOT_CHN]; //[mult]
+  UInt_t mult;                  // number of strips with deposit
+  Double_t Ampnth[FOOT_CHN];    //[mult]
+  Int_t strip[FOOT_CHN];        //[mult]
   UInt_t clmult;
-  Int_t  clfirst[FOOT_CHN]; //[clmult] first strip in the cluster
-  Int_t  cllast[FOOT_CHN]; //[clmult] last strip in the cluster
-  Double_t  clE[FOOT_CHN]; //[clmult]
-  Double_t  clpos[FOOT_CHN]; //[clmult]
+  Int_t clfirst[FOOT_CHN];  //[clmult] first strip in the cluster
+  Int_t cllast[FOOT_CHN];   //[clmult] last strip in the cluster
+  Double_t clE[FOOT_CHN];   //[clmult]
+  Double_t clpos[FOOT_CHN]; //[clmult]
 
-  ClassDef(TFOOTContainer,1)
+  ClassDef(TFOOTContainer, 1)
 };
 
-
-#endif //TFOOTCONTAINER_H
-
+#endif // TFOOTCONTAINER_H
